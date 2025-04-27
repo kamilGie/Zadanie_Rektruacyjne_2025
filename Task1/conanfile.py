adfa2805b7bb9c8d63ac8b4318c0a2e16af55554
Kamil Gieras <kamil.gieras@onet.pl>
2025-04-27 21:22:44 +0200

    UPDATE

--- Task1/conanfile.py
+++ Task1/conanfile.py
@@ -8,6 +8,10 @@ class Task1Recipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
+    def configure(self):
+        if not self.settings.build_type:
+            self.settings.build_type = "Release"
+
     def requirements(self):
         self.requires("gtest/1.16.0")
 
